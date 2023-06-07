7c49305b741fae8ec0904dd71c30b2545900a2dc
10takla <letifovabakar98@gmail.com>
2023-06-07 13:04:24 +0300

    offet

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-**9@i_vn&gg66-)fu0$^a!a6@q3u(#h34^yu$gkq7dkd@tqlll
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
